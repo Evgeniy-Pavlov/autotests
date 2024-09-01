8ec2cd9a514dc606f61a2447fe99f70b1b2d6604
Evgeniy-Pavlov <pavlovev@it2g.ru>
2024-09-01 20:58:25 +0300

    сделал распределение книг между пользователями

--- src/second_FP/main.py
+++ src/second_FP/main.py
@@ -1,5 +1,7 @@
 import os
+import csv
 import json
+import datetime
 import argparse
 
 def parse_arguments():
@@ -8,13 +10,34 @@ def parse_arguments():
     parser.add_argument('-b', type=str, help='Путь до файла со списком книг. Файл должен быть обязательно в формате csv', default='./books.csv')
     return parser.parse_args()
 
+
+def read_json(file_path):
+    with open(file_path, 'r') as f:
+        users_dict = json.load(f)
+    return users_dict
+
+
+def read_csv(file_path):
+    with open(file_path, newline='') as f:
+        result = list(csv.DictReader(f))
+    return result
+
+
 def main():
     arg_parse = parse_arguments()
     users_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), arg_parse.u))
-    print(arg_parse.u)
-    with open(users_file_path, 'r') as f:
-        users_dict = json.load(f)
-        
+    books_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), arg_parse.b))
+    list_users = read_json(users_file_path)
+    list_books = read_csv(books_file_path)
+    for e, book in enumerate(list_books):
+        user = list_users[e%len(list_users)]
+        if not user.get('books'):
+            user['books'] = [book]
+        else:
+            user['books'].append(book)
+    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), f'result_{datetime.datetime.now().date()}.json')), "w") as f:
+        json.dump(list_users, f)       
+
 
 if __name__ == '__main__':
     main()
